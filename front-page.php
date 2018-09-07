9d9ad24b7717324efc53c030060cde18d35ec084
Jeanie  Orejarena <jeanie@crossfield.com>
2018-09-07 04:50:53 -0400

    Fixing front page

--- front-page.php
+++ front-page.php
@@ -12,35 +12,33 @@ get_header()?>
 <?php if ( have_posts() ) : ?>
 	<main class="page-wrapper">
 		<div id="home" class="hero">
-			<img src="https://via.placeholder.com/1436x960?text=Hello+World">
+			<img src="https://via.placeholder.com/1436x100?text=Hello+World">
 		</div>
 		<!-- ABOUT SECTION -->
 		<div id="about" class="about-container">
-			<!-- <div class="triangle"></div> -->
 			<div class="section">
-				<?php
-					echo get_template_part( 'partials/about', 'intro' );
-				?>
+				<?php echo get_template_part( 'partials/about', 'intro' ); ?>
 			</div>
 			<div class="about-list-section section"  style="background-image:url(https://placeimg.com/343/537/arch)">
 				<div class="about-lists-container container">
 					<?php echo get_template_part( 'partials/about', 'lists' ); ?>
 				</div>
-
 			</div>
 		</div>
 		<!-- ABOUT SECTION -->
 		<!-- EVENT SECTION -->
 		<div id="events" class="events-container">
 			<div class="section">
-				<?php
-					echo get_template_part( 'partials/events', 'intro' );
-					echo Views\build_events_intro_list();
-					echo Helpers\button( 'Apply', '', 'test');
-				?>
+				<div class="container">
+					<?php
+						echo get_template_part( 'partials/events', 'intro' );
+						echo Views\build_events_intro_list();
+						echo Helpers\button( 'Apply', '', 'test');
+					?>
+				</div>
 			</div>
 			<?php
-				// echo Views\build_events_slider();
+				echo Views\build_events_slider();
 				echo get_template_part( 'partials/testimonials', 'slider' );
 			?>
 			<div class="events-schedule section"></div></div>
@@ -55,7 +53,6 @@ get_header()?>
 					echo Helpers\button( 'Load More', 'white', 'loadmore');
 				?>
 			</div>
-
 		</div>
 		<!-- MEDIA SECTION -->
 		<!-- CONTACT SECTION -->
@@ -68,10 +65,6 @@ get_header()?>
 			</div>
 		</div>
 		<!-- CONTACT SECTION -->
-		<?php
-			echo get_template_part( 'partials/modal', 'modal' );
-		?>
-
 	</main>
 <?php endif; ?>
 <?php get_footer(); ?>
