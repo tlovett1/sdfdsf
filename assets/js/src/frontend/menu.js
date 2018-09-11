60bd9c1fe9f0aa5173bfc2377c97ad94ed572075
Jeanie  Orejarena <jeanie@crossfield.com>
2018-09-11 12:48:29 -0400

    Fixing about list color

--- assets/js/src/frontend/menu.js
+++ assets/js/src/frontend/menu.js
@@ -1,11 +1,13 @@
 export default () => {
   const siteMenu = document.getElementById('siteMenu');
-  const menuButton = document.getElementById('menuButton');
-  const html = document.documentElement;
 
   if (!siteMenu) {
     return false;
   }
+
+  const menuButton = document.getElementById('menuButton');
+  const menuItems = siteMenu.querySelectorAll('.menu-item');
+  const html = document.documentElement;
 
   function closeMenu() {
     html.classList.remove('menu-open');
@@ -22,6 +24,15 @@ export default () => {
       } else {
         openMenu();
       }
+    });
+
+    [].forEach.call(menuItems, menuItem => {
+      const link = menuItem.querySelector('a');
+
+      link.addEventListener('click', event => {
+        event.preventDefault();
+        closeMenu();
+      });
     });
   }
 
