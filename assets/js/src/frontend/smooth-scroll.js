e7c082d761a6ff47e48bb173465910aa0af473e7
Jeanie  Orejarena <jeanie@crossfield.com>
2018-09-11 17:46:51 -0400

    setting the offset to the header height and bumping up the version number

--- assets/js/src/frontend/smooth-scroll.js
+++ assets/js/src/frontend/smooth-scroll.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const headerHeight = document
+    .getElementById('siteHeader')
+    .getBoundingClientRect().height;
+
   gumshoe.init({
     selector: '.menu-item a[href*="#"]',
     selectorHeader: 'header',
-    offset: 80,
+    offset: headerHeight,
     activeClass: 'active',
     scrollDelay: false,
   });
